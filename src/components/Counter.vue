9e8af2ac678014ba7ffd5c758aa73bd57e538b5c
Vasco Neves <vasco.neves98@gmail.com>
2022-08-12 11:26:38 +0100

    Setup project(eslint prettier and vscode)

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ displayDays }}
+    {{ displayHours }}
+    {{ displayMinutes }}
+    {{ displaySeconds }}
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      displayDays: 0,
+      displayHours: 0,
+      displayMinutes: 0,
+      displaySeconds: 0,
+    };
+  },
+  methods: {},
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from "@/components/Counter.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Counter,
+  },
+};
+</script>
